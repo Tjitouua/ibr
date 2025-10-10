7364540a79a0a27ae994183d63dfd6590be563d1
Tjitouua <veriimapoha@gmail.com>
2025-10-10 15:18:59 +0200

    Made changes to the Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import { IoMenu } from "react-icons/io5";
 import { RxCross2 } from "react-icons/rx";
+import { FiPhone } from "react-icons/fi";
+import { MdOutlineMail } from "react-icons/md";
 
 const Header = () => {
 
@@ -12,7 +14,8 @@ const Header = () => {
         <header className="w-full flex flex-col gap-3 sticky top-0 z-50">
         <div className="w-full min-h-15 flex flex-col gap-2">
             {/* Top Header  */}
-            <div className="w-full flex bg-white items-center gap-4 min-h-10 py-1">
+            <div className="w-full flex bg-white items-center flex-wrap justify-between gap-4 min-h-10 py-1">
+                <div className="flex gap-6 flex-wrap">
                 {/* Image Div  */}
                 <div className="w-25 sm:w-15 h-25 sm:h-15">
                    <img src="/images/namibiaLogo.png" className="w-full h-full" />
@@ -21,6 +24,20 @@ const Header = () => {
                 <div className="min-h-15 flex flex-col justify-start pt-1">
                     <label className="font-semibold text-lg">NAMIBIA INTEGRATED BENEFICIARY REGISTRY (IBR)</label>
                     <label className="text-sm">Ministry of Gender Equality, Poverty Eradication and Social Welfare</label>
+                </div>
+                </div>
+                {/* Contact div  */}
+                <div className="flex flex-col text-sm">
+                    {/* Phone  */}
+                    <div className="flex gap-2 items-center">
+                        <FiPhone />
+                        <label>264 61 283 3111</label>
+                    </div>
+                    {/* Email  */}
+                    <div className="flex gap-2 items-center">
+                       <MdOutlineMail />
+                       <label>info@ibr.gov.na</label>
+                    </div>
                 </div>
             </div>
             {/* Bottom Header  */}
