70ae122963c1e0d14bea9605da5b78902914ffc7
Tjitouua Mapoha <tmapoha@GRN.GOV.NA>
2025-10-13 11:12:00 +0200

    Added the hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => {
+     return (
+       <div className="w-full flex items-center flex-col bg-[url('/images/hero.jpeg')] bg-center bg-cover min-h-170">
+
+       </div>
+     );
+}
+
+export default Hero;
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Hero from '../components/Hero';
 
 const Index = () => {
     return (
         <div className="w-full min-h-screen px-7 xl:px-20">
             <Header />
-             <div className="w-full min-h-screen">
-
-             </div>
+             <Hero />
              <Footer />
         </div>
     )
