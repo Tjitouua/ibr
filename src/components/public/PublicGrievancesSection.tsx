268fef29f0e60bc26a525632a0278ade0d5d2257
Tjitouua <veriimapoha@gmail.com>
2025-12-09 11:00:56 +0200

    Busy with the Status Verification part

--- src/components/public/PublicGrievancesSection.tsx
+++ src/components/public/PublicGrievancesSection.tsx
@@ -1,9 +1,7 @@
-import ContactInfo from "../../ui/public/ContactInfo";
-import ContactUsForm from "../../ui/public/ContactUsForm";
+import { FaSearch } from "react-icons/fa";
 import GrievancesForm from "../../ui/public/GrievancesForm";
 import PublicSectionsDiv from "../../ui/public/PublicSectionsDiv";
-import RegionalOffices from "../../ui/public/RegionalOffices";
-import FAQs from "./FAQs";
+
 
 
 
@@ -14,6 +12,19 @@ const PublicGrievancesSection = () => {
             <label className="text-gray-600">If you’re a beneficiary and something isn’t right, just let us know. Fill in the form and we’ll sort it out.</label>
             <div className="w-full flex-wrap min-h-20 flex gap-5 justify-between items-start py-6">
                 <GrievancesForm />
+                <div className="w-full lg:w-[35%] flex flex-col gap-4">
+                    <div className="w-full py-8 px-6 rounded-md border gap-2 flex flex-col border-gray-300">
+                       <label className="font-bold text-xl">Beneficiary Verification</label>
+                       <label className="text-sm text-gray-400">Enter your details below to check if you are a beneficiary</label>
+                       {/* ID Number  */}
+                       <div className="w-full py-2 flex flex-col gap-3 text-black/80">
+                           <label className="font-bold text-sm">ID Number or Program Number</label>
+                           <input className="w-full border border-gray-300 bg-gray-100 py-2 px-3" type="text" placeholder="Enter your ID number (e.g. 8976234829)" />
+                       </div>
+                       {/* Button  */}
+                       <button className="bg-black text-white rounded-sm gap-3 px-2 py-3 flex justify-center items-center hover:bg-black/70 cursor-pointer w-full mt-3">Verify Status <FaSearch /></button>
+                    </div>
+                </div>
             </div>
         </PublicSectionsDiv>
      );
